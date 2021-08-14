be22e7a80160cc198af6984e3eade19a8a8f439e
Kazuroni <sayajinbr30@gmail.com>
2021-08-14 19:46:50 -0300

    AranhaFinalizada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Não Esmague a aranha,cuidado");
+            Console.ReadLine();
+            
+            Console.WriteLine("Atenção!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Você não esmagou a aranha parabens :D");
+        }
+    }
+}
